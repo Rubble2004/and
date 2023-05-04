c41c1c287075381e9788a0ac5ab511c7591ca98c
Dmitry <dmitrie.eremin.04@gmail.com>
2023-05-04 12:39:32 +0300

    final

--- app/src/main/java/com/example/olman/sortirovka.java
+++ app/src/main/java/com/example/olman/sortirovka.java
@@ -3,6 +3,8 @@ package com.example.olman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class sortirovka extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class sortirovka extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sortirovka);
+        VideoView videoView = findViewById(R.id.sort);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.kontrol);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
     }
 }
